04b661c47adef77941434e00078db0b1e72c1be2
Erickson7 <ericksonemmanuel280@gmail.com>
2026-05-21 20:17:31 +0300

    Added requirements.txt

--- ussd_finance_system/ussd_finance_system/sms.py
+++ ussd_finance_system/ussd_finance_system/sms.py
@@ -1,8 +1,9 @@
 import africastalking
+import os
 
-username = "sandbox"
+username = os.environ.get("sandbox")
 
-api_key = "atsk_95425448b131a2dde35a870999f079e6c047e01721f375f615914c22fe4b115066582696"
+api_key = os.environ.get("atsk_95425448b131a2dde35a870999f079e6c047e01721f375f615914c22fe4b115066582696")
 
 africastalking.initialize(
     username,
